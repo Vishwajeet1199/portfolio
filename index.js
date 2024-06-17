d04fc33c62a24af9b02c67f0b2fada770713c593
Vishwajeet Jadhav <vishwjeet111@gmail.com>
2024-06-17 22:58:37 +0530

    edit home page and add about page and chat functionlity

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links=document.querySelectorAll('.nav-link')
-    links.forEach(link=>{
-        link.addEventListener('click',function(e){
-            e.preventDefault()
-            links.forEach(l=>{
-               l.classList.remove('active');
-               this.classList.add('active')
-            }) 
-        })
-    })
-   
-})
+
+var typed = new Typed('.auto-type', {
+    strings: ['Vishwajeet .', 'a Web Developer.'],
+    typeSpeed: 150,
+    backSpeed: 100, 
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+    cursorChar: '|',
+  });
+
+  function toggleChat() {
+    const chatBox = document.getElementById('chat-box');
+    chatBox.style.display = chatBox.style.display === 'none' || chatBox.style.display === '' ? 'flex' : 'none';
+
+}
